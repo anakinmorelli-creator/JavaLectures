71ada3f3065ec68be35ba01c6048bd155916d522
Anakin Morelli <anakinmorelli@gmail.com>
2026-03-04 16:23:02 -0500

    Added all lectures and messing around in lecture 2

---
+++ Lecture2/L2.java
@@ -0,0 +1,17 @@
+class L2 {
+    int x;
+    int y;
+
+    // The Constructor
+    L2(int startX, int startY) {
+        x = startX;
+        y = startY;
+    }
+
+    // Calling the constructor using the 'new' keyword
+    L2 p1 = new L2(10, 20);
+
+    System.out.println();
+}
+
+
