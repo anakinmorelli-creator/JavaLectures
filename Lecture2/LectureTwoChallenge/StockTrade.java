557fb41a1293777a6378c1ec5571853622beaf3a
Anakin Morelli <anakinmorelli@gmail.com>
2026-03-04 23:08:02 -0500

    starting lecture 2

---
+++ Lecture2/LectureTwoChallenge/StockTrade.java
@@ -0,0 +1,29 @@
+public class StockTrade {
+    private String tickerSymbol;
+    private Double tradePrice;
+
+    StockTrade(String symbol, Double price){
+        tickerSymbol = symbol;
+        tradePrice = price;
+    }
+
+    public String getTickerSymbol() {
+        return tickerSymbol;
+    }
+
+    public Double getTradePrice(){
+        return tradePrice;
+    }
+
+    public String toString(){
+        return "Trade executed for " + getTickerSymbol() + " at $" + getTradePrice();
+    }
+
+    public static void main(String[] args){
+        StockTrade sofiStock = new StockTrade("SOFI", 18.70);
+        StockTrade rivnStock = new StockTrade("RIVN", 14.92);
+
+        System.out.println(sofiStock);
+        System.out.println(rivnStock);
+    }
+}
